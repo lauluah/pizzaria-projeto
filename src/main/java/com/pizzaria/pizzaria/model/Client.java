5ebaf352afb5421e6bde9f453b44a29a03e235d9
Ana Laura Marques <lauramarques181@gmail.com>
2025-05-06 20:22:14 -0300

    first commit

---
+++ src/main/java/com/pizzaria/pizzaria/model/Client.java
@@ -0,0 +1,78 @@
+package com.pizzaria.pizzaria.model;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "CLIENT")
+public class Client {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "birthDate", nullable = false)
+    private LocalDate birthDate;
+
+    @Column(name = "phone", nullable = false)
+    private String phone;
+
+    @Column(name = "address", nullable = false)
+    private String address;
+
+    public Client(Long id, String name, LocalDate birthDate, String phone, String address) {
+        this.id = id;
+        this.name = name;
+        this.birthDate = birthDate;
+        this.phone = phone;
+        this.address = address;
+    }
+
+    public Client() {
+
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public LocalDate getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(LocalDate birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+}
---
+++ src/main/java/com/pizzaria/pizzaria/repository/ClientRepository.java
@@ -0,0 +1,15 @@
+package com.pizzaria.pizzaria.repository;
+
+import com.pizzaria.pizzaria.model.Client;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Optional;
+
+public interface ClientRepository extends JpaRepository<Client, Long> {
+    Optional<Client> findByPhone(String phone);
+    List<Client> findByName(String name);
+    List<Client> findByLastName(String lastName);
+    Optional<Client> findByBirthDate(LocalDate birthDate);
+}
