bce83e3bbbddebc3e43739b4f1ea04fad630c044
Ana Laura Marques <lauramarques181@gmail.com>
2025-05-15 11:26:02 -0300

    add buscar clientes

--- src/main/java/com/pizzaria/pizzaria/repository/ClientRepository.java
+++ src/main/java/com/pizzaria/pizzaria/repository/ClientRepository.java
@@ -2,13 +2,8 @@ package com.pizzaria.pizzaria.repository;
 
 import com.pizzaria.pizzaria.model.Client;
 import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.time.LocalDate;
 import java.util.List;
-import java.util.Optional;
 
 public interface ClientRepository extends JpaRepository<Client, Long> {
-    Optional<Client> findByPhone(String phone);
     List<Client> findByNameContainingIgnoreCase(String name);
-    Optional<Client> findByBirthDate(LocalDate birthDate);
 }
